bd4b1a4aae9a2f65bf2e76b6fa8936a2bf56c00e
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2024-02-15 20:08:25 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function HashMap() {
+  const hash = (key) => {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode %= primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+
+  }
+
+  return { hash };
+}
